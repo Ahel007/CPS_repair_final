867b7b89976e33a8d8191a5459853e19ebd081e8
Ahel007 <magammg2002@mail.ru>
2024-12-14 19:57:44 +0300

    complete

---
+++ webpack-static-template/src/js/index.js
@@ -0,0 +1,15 @@
+import '../scss/style.scss'
+import '../js/modal.js'
+import '../js/repaired-devices'
+import '../js/services'
+import '../js/sidebar'
+import '../js/swiper-initial'
+
+
+
+
+
+
+
+
+
---
+++ webpack-static-template/src/js/swiper-initial.js
@@ -0,0 +1,19 @@
+new Swiper('.swiper', {
+    cssMode: 'true',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        enabled: true,
+        slidesPerView: 1.15,
+        width: 326
+      },
+      768: {
+        enabled: false,
+        slidesPerView: 'auto',
+        spaceBetween: 'auto'
+      }
+    }
+  })
